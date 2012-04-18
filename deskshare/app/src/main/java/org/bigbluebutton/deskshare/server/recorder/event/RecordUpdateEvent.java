def449dde7ade5b117ff23adce95c0ed597340c0
Justin Robinson <jtrobinson629@gmail.com>
2012-04-18 14:31:34 -0400

    Repo clean; ready for a fresh start with a fresh merge

--- deskshare/app/src/main/java/org/bigbluebutton/deskshare/server/recorder/event/RecordUpdateEvent.java
+++
@@ -1,7 +0,0 @@
-package org.bigbluebutton.deskshare.server.recorder.event;
-
-public class RecordUpdateEvent extends AbstractDeskshareRecordEvent {
-	public RecordUpdateEvent(String session) {
-		super(session);
-	}
-}
